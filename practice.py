6eeb4945e6075598693ab109b07b4a6dea47c133
Kishiannn_ <abudachan122@gmail.com>
2025-03-16 19:43:52 +0800

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+mynum = 10;
+print(mynum)
+
+sum = mynum + mynum
+print(sum)
+
+print("mynum + mynum is: ", sum);
+print("This is a very long line of comment i want it to divide it into" +
+      " two section for better readability");
+
+#this is a comment
+## this is also a comment
+'''
+    This is also a comment 
+    that i learned 
+    Amazing 
+'''
