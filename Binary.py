bd062ac0ff3e56bb1c9e5b33c9b39d5d76304bee
Kishiannn_ <abudachan122@gmail.com>
2025-03-16 20:34:46 +0800

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -7,3 +7,15 @@ print("100 Binary", Test2)
 print("100 Binary", Test3)
 print("100 Binary", Test4)
 print(bin(Test))
+
+## Another Version of Code
+
+Test = 0b1
+print("100 Binary", Test)
+Test = 0b10
+print("100 Binary", Test)
+Test = 0b11
+print("100 Binary", Test)
+Test = 0b100
+print("100 Binary", Test)
+print(bin(Test))
