909f0d9858736a6727ff07624a3d50a38d66485d
Kishiannn_ <abudachan122@gmail.com>
2025-03-16 20:32:50 +0800

    Create Binary.py

---
+++ Binary.py
@@ -0,0 +1,9 @@
+Test1 = 0b1
+Test2 = 0b10
+Test3 = 0b11
+Test4 = 0b100
+print("100 Binary", Test1)
+print("100 Binary", Test2)
+print("100 Binary", Test3)
+print("100 Binary", Test4)
+print(bin(Test))
