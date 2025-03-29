8487de6e329d6abb2e122787da15a377ebf82f59
Kishiannn_ <abudachan122@gmail.com>
2025-03-29 21:50:30 +0800

    Create notFinishYet.py

---
+++ notFinishYet.py
@@ -0,0 +1,7 @@
+num1 = input("Please enter the first number: ")
+print(num1)
+num2 = input("Please enter the second number: ")
+print(num2)
+def total(num1, num2):
+    return(num1 + num2)
+print(total)
