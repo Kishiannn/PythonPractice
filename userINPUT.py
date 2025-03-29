2da5dac5aec37cd194c33171da09e8f03d04fe9d
Kishiannn_ <abudachan122@gmail.com>
2025-03-29 21:39:49 +0800

    Create userINPUT.py

---
+++ userINPUT.py
@@ -0,0 +1,12 @@
+Name = input("What is your name?")
+  #The system asking the name of the user
+print("My Name is: ", Name)
+  #User input there name
+Age = input("How old are you?")
+ #The system asking the age of the user
+print("Im ", Age, " Year's old")
+  #User input there age
+Course = input("What is your course")
+ #The system asking the course of the user
+print("My course is: ", Course)
+ #User input there course
