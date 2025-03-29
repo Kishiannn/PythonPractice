cf72de2a6147670acb4ee08822367bd3abbef849
Kishiannn_ <abudachan122@gmail.com>
2025-03-29 23:27:38 +0800

    Update and rename notFinishYet.py to BasicAddition.py

--- BasicAddition.py
+++ BasicAddition.py
@@ -2,6 +2,7 @@ num1 = input("Please enter the first number: ")
 print(num1)
 num2 = input("Please enter the second number: ")
 print(num2)
-def total(num1, num2):
-    return(num1 + num2)
-print(total)
+
+
+sum = int(num1) + int(num2)
+print (sum)
